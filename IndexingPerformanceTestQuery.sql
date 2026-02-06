d5b948f52f248f89f8888488b78cccbc839c617c
Abhinandan Sah <abhinandansah222@gmail.com>
2026-02-06 14:50:48 +0530

    update

--- IndexingPerformanceTestQuery.sql
+++ IndexingPerformanceTestQuery.sql
@@ -18,6 +18,7 @@ FROM
 perf_issure;
 */
 
+GO
 select
 	so.name, 
 	--ps.*,
@@ -28,3 +29,25 @@ inner join
 on ps.object_id= so.id
 where so.xtype = 'U'
 ORDER BY used_page_count desc;
+
+GO
+
+USE AdventureWorks2022;
+GO
+
+DROP TABLE IF EXISTS dbo.SOH_Practice;
+SELECT TOP (300000)
+  SalesOrderID, CustomerID, OrderDate, SubTotal, TaxAmt, Freight, TotalDue
+INTO dbo.SOH_Practice
+FROM Sales.SalesOrderHeader
+ORDER BY SalesOrderID;
+
+-- Create a clustered index on SalesOrderID (common pattern)
+-- This leaves our search columns (CustomerID, OrderDate) without a supporting index.
+CREATE CLUSTERED INDEX CX_SOH_Practice_SalesOrderID
+ON dbo.SOH_Practice(SalesOrderID);
+
+GO
+select * from dbo.SOH_Practice;
+GO
+
